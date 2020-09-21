c943df7920bb2b0c6cbb4e36fe0e71116326d34e
sanggoe <sanggoe0509@gmail>
2020-09-21 13:07:55 +0900

    edit

--- src/main/java/com/sanggoe/hellospring/controller/MemberController.java
+++ src/main/java/com/sanggoe/hellospring/controller/MemberController.java
@@ -4,8 +4,11 @@ import com.sanggoe.hellospring.domain.Member;
 import com.sanggoe.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -30,5 +33,12 @@ public class MemberController {
         memberService.join(member);
 
         return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findeMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
     }
 }
